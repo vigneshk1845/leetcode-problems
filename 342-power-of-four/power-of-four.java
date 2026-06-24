50d7e66c54aaa44e4644ecf45075807b31a9ec1a
vigneshk1845 <renugadevi792@gmail.com>
2026-06-24 12:00:59 +0530

    Time: 1 ms (89.95%) | Memory: 42.7 MB (32.04%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<=0)
-        return false;
+        if(n<=0) return false;
         while(n%4==0){
-         n/=4;
-        }
-        return n==1;
+            n/=4;
+        }return n==1;
     }
 }
