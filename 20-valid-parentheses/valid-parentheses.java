51abb245d00b33022ed8a7dbc3c58724d743b2cb
vigneshk1845 <renugadevi792@gmail.com>
2026-06-24 11:03:53 +0530

    Time: 3 ms (87.50%) | Memory: 43.1 MB (84.94%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
-        Hmap.put(')','(');
-        Hmap.put('}','{');
-        Hmap.put(']','[');
-        Stack<Character> stack = new Stack<Character>();
-        for (int idx = 0; idx < s.length(); idx++){
-            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
-                stack.push(s.charAt(idx));
-                continue;
-            }
-            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
-                return false;
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
             }
         }
-        if (stack.size() == 0) {
-            return true;
-        }
-        return false;
+        return stack.isEmpty();
     }
 }
